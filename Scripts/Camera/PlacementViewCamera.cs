097d2df985dfc2428b021dc920ab1970b3ce05fb
Ben Baker <40375989+BensBaker35@users.noreply.github.com>
2023-02-06 13:01:25 -0500

    Custom Camera that extends ArcGISCameraComponent

---
+++ Scripts/Camera/PlacementViewCamera.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using Esri.ArcGISMapsSDK.Components;
+using Esri.ArcGISMapsSDK.Utils.GeoCoord;
+using Esri.GameEngine.Geometry;
+using Esri.GameEngine.MapView;
+using RIT.RochesterLOS.Control;
+using RIT.RochesterLOS.Player;
+using UnityEngine;
+
+namespace RIT.RochesterLOS.CustomCamera
+{
+    public class PlacementViewCamera : ArcGISCameraComponent, IControllable
+    {
+
+        void Awake()
+        {
+            base.Initialize();
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        protected override void Update()
+        {
+            //Input.Scr
+            base.Update();
+        }
+
+        public void PushKeyboardInput(Vector3 input)
+        {
+            transform.Translate(input.x, 0, -input.y, Space.World);
+        }
+
+        public void PushMouseInput(Vector3 look, Vector2 scroll)
+        {
+            if(Input.GetMouseButton(1)) //Right Click
+            {
+                transform.Rotate(Vector3.forward * -look.x);
+            }
+        }
+    }
+}
